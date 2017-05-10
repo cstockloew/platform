90d9e41c50901a88c9f3b03f901831a883aa1409
cstockloew <stockloew@gmx.de>
2017-05-10 03:58:33 +0200

    testing site

--- push-to-gh-pages.sh
+++ push-to-gh-pages.sh
@@ -4,21 +4,19 @@ if [ "$TRAVIS_REPO_SLUG" == "cstockloew/platform" ] && [ "$TRAVIS_PULL_REQUEST" 
 
   echo -e "Publishing...\n"
 
-  cp -R "target/site/apidocs" $HOME/javadoc-latest
-
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/cstockloew/platform gh-pages > /dev/null
 
   cd gh-pages
-  git rm -rf ./javadoc > /dev/null
-  cp -Rf $HOME/javadoc-latest ./javadoc
+  git rm -rf ./site > /dev/null
+  cp -Rf $HOME/site ./site
   git add -f . > /dev/null
-  git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"  > /dev/null
+  git commit -m "Latest site on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"  > /dev/null
   git push -fq origin gh-pages > /dev/null
 
-  echo -e "Published Javadoc to gh-pages.\n"
+  echo -e "Published site to gh-pages.\n"
   
 fi
 
